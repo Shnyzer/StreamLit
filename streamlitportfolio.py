9cc1743ca0546717a81c090a25a707a3b9ff7b6a
Felix <143820204+Shnyzer@users.noreply.github.com>
2024-08-30 21:34:18 +0800

    Update streamlitportfolio.py

--- streamlitportfolio.py
+++ streamlitportfolio.py
@@ -1,21 +1,6 @@
-import streamlit as st 
-import numpy as np
-import pandas as pd 
-from streamlit_option_menu import option_menu
-import requests
-from streamlit_lottie import st_lottie
-from PIL import Image
+import streamlit as st
 
-st.set_page_config(layout= "wide")
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-lottie_code = load_lottieurl ("https://lottie.host/677afcc3-42fb-4602-a75a-6d754897c22b/pFsY797vh1.json")
-image = Image.open("C:\\Users\\User\\Downloads\\MyFirstStreamLitApp\\Hirehub.png")
+st.set_page_config(layout="wide")
 
 
 st.title("My First Streamlit App")
@@ -23,52 +8,84 @@ st.title("My First Streamlit App")
 st.write("##")
 st.subheader("Hello Everyone :wave: ")
 st.title("My Portfolio")
-st.write("Strong Ability to gather and synthesize information while communicating results effectively and concisely in support of cooperate efforts")
+st.write("Strong ability to gather and synthesize information while communicating results effectively and concisely in support of cooperative efforts.")
+
+tabs = st.tabs(["Personal Data", "Skills", "Languages"])
+
+with tabs[0]:
+    st.write("""
+             - Name: Felix C. Vilocura II
+             - Gender: Male
+             - Civil Status: Single
+             - Birthday: March 13, 2000
+             - Birthplace: Cebu City
+             - Region: Roman Catholic
+             - Father: Felix C. Vilocura
+             - Occupation: Civil Engineer, Business Management
+             - Mother: Juliet C. Vilocura
+             - Occupation: HouseWife""")
+with tabs[1]:
+    st.write("""
+             - Open-Mindedness
+             - Creativity
+             - Motivation and Perseverance
+             - Problem Solving and Resourcefulness
+             - Critical Thinking
+             - Leadership
+             - Teamwork
+             - Adaptibility""")
+with tabs[2]:
+    st.write("""
+             - English
+             - Tagalog
+             - Cebuano 
+             """)
+    
 st.write("---")
 
+st.title("Autobiography")
+st.write("""
+    I am Felix C. Vilocura II, born on March 13, 2000, and I live at 91-A F. Llamas St., Punta Princesa, Cebu City.
+    I have been attending Cebu Institute of Technology - University since Kindergarten and am currently pursuing a Bachelor of Science in Information Technology.
+    I have 5 brothers, and I am the third among them. My father is a Civil Engineer, and my mother, who has a background in Tourism, chose to be a housewife to guide us.
+""")
+
+st.write("""
+    As a child growing up in Cebu City, I was fortunate to attend a well-regarded private school from kindergarten through high school and college.
+    The people I met in my life instilled a strong educational and ethical philosophy that remains with me today. This foundation, along with the support of my family, educators, and friends, has helped me achieve my dreams.
+    My favorite types of music are E.M.O and RAP, and my favorite foods include vegetables, calamares, and lechon. My favorite colors are dark colors.
+""")
+
+st.write("""
+    One of my most unforgettable moments was during Senior High School when I had a lot of fun with my friends. We would often cut classes to play computer games, basketball, drink, or play billiards. Despite my grades suffering due to my lack of studying, I relied on my ability to remember discussions from school.
+    I've learned from these experiences and have managed to stay on track despite the mistakes.
+""")
+
+col1, col2 = st.columns(2)
+
 with st.container():
-    selected = option_menu(
-        menu_title = None,
-        options = ['About', 'Projects', 'Contact'],
-        default_index=0,
-        icons = ['person', 'code-slash', 'chat-left-text-fill'],
-        orientation= 'horizontal',
-    )
+    with col1:
+        check1 = st.checkbox("Education")
 
-if selected == 'About':
-    
-    with st.container():
-        col1, col2 = st.columns(2)
-        with col1:
-            st.write("##")
-            st.subheader("I am Felix C. Vilocura II")
-            st.title("BSIT Student")
-        with col2:
-            st.write("##")
-            st_lottie(lottie_code)
+        if check1:
+            
+            st.write("""
+                - Cebu Institute of Technology - University
+                    - College - Bachelor of Science in Information Technology (Recent)
+                - Cebu Institute of Technology - University
+                    - Senior High School (2017 - 2019)
+                - Cebu Institute of Technology - University
+                    - Junior High School (2013 - 2017)
+                - Cebu Institute of Technology - University
+                    - Elementary (2007 - 2013)
+            """)
 
-    st.write("---")
-    
-    with st.container():
-        col3, col4 = st.columns(2)
-        with col3:
-            st.subheader("""
-            Education
-            - Cebu Institute of Technology - University
-                -   College - Bachelor of Science Information of Technology 
-                -   Recent 
-            - Cebu Institute of Technology - University 
-                -   Senior High School 
-                -   2017 - 2019
-            - Cebu Institute of Technology - University 
-                -   Junior High School 
-                -   2013 - 2017
-            - Cebu Institute of Technology - University
-                -   Elementary
-                -   2007 - 2013
-                """)
-        with col4:
-            st.subheader("""
+    with col2:
+        check2 = st.checkbox("Awards / Achievements")
+
+        if check2:
+            
+             st.write("""
             Awards / Achievement
             -   March 09, 20222 
                 -   Course Certificate - HCIP
@@ -85,25 +102,3 @@ if selected == 'About':
             -   November 05, 2023 
                 -   Certificate Kaggle - Data Visualization
                 """)
-if selected == 'Projects':
-    with st.container():
-        st.header("My Projects")
-        st.write("##")
-        col5, col6 = st.columns({1,2})
-        with col5:
-            st.image(image)
-        with col6:
-            st.subheader("HireHub")
-            st.write("""HireHub: Cebu Institute of Technology - University 
-                    IT-Enabled 
-                    Applicant Tracking 
-                    System""")
-
-elif selected == 'Contact':
-    st.header("Contact Me")
-    st.write("You can reach me at my email: fvilocuraii@gmail.com")
-    
-
-
-
-        
